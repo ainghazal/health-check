f248726d8c3f4ec8122fb5cfba8bb60dd3b74640
Ain Ghazal <ainghazal42@gmail.com>
2022-09-02 10:20:18 +0200

    set production values

--- config.go
+++ config.go
@@ -8,11 +8,9 @@ import "time"
 
 // TODO make viper able to set all these vars
 var (
-	runInterval           = 1 * time.Minute
-	maxEndpointsToTest    = -1 // DEBUG, change-me for release (-1 for all).
-	pingCount             = 3  // DEBUG this should be lower for debug, 5-10 for prod.
-	maxConcurrentCheckers = 10
-
-	// TODO(ainghazal) the granularity in here is too big with n=5. We could raise to n=10 and reduce the inter-ping time to keep the measurement "fast".
+	maxConcurrentCheckers      = 20
+	runInterval                = 10 * time.Minute
+	pingCount                  = 5   // DEBUG this should be lower for debug, 5-10 for prod.
+	maxEndpointsToTest         = -1  // DEBUG, change-me for release (-1 for all).
 	healthThresholdForPingLoss = 0.8 // below this loss rate, it's healthy
 )
